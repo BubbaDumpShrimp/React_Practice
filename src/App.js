d50a565fd0d61ce581f03628efa43d3b9464171e
BubbaDumpShrimp <mikel.piano95@gmail.com>
2020-08-02 21:18:20 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+
+let defaulttextcolor = "red";
+let defaultStyle = {
+ color: defaulttextcolor 
+};
+
+
+function Aggregate() {
+  return(
+    <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
+      <h2>Number Text</h2>
+    </div>
+  );
+}
+
+
+function Filter() {
+  return(
+    <div style={defaultStyle}>
+      <input type="text"/>
+    </div>
+  );
+}
+
+function Playlist(){
+  return(
+    <div style={{...defaultStyle, display: "inline-block", width: "30%"}}>
+      <h3>Playlist Name</h3>
+      <ul><li>Song1</li><li>Song3</li><li>Song1</li></ul>
+    </div>
+  );
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Title</h1>
+      <Aggregate/>
+      <Aggregate/>
+      <Filter/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
+    </div>
+  );
+}
+
+export default App;
